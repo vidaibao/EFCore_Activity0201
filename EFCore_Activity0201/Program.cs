379885704564d27265591aed79002ff5976fed31
vidaibao <vutrungtin@gmail.com>
2024-02-09 11:46:17 +0900

    Add project files.

---
+++ EFCore_Activity0201/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore_Activity0201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ EFCore_DBLibrary/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DBLibrary
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Add a default constructor if scaffolding is needed
+        public ApplicationDbContext() { }
+
+        //Add the complex constructor for allowing Dependency Injection
+        public ApplicationDbContext(DbContextOptions options)        : base(options)
+        {
+            //intentionally empty.
+        }
+
+
+
+    }
+}
